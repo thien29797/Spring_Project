0899d8e970c4f3ba884b2031050734e28c930bbe
thien29797 <thien2971997@gmail.com>
2019-06-04 14:51:24 +0700

    first commit

---
+++ SpringBootJWT/src/main/java/controller/MainRESTController.java
@@ -0,0 +1,100 @@
+package controller;
+
+import java.awt.PageAttributes.MediaType;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import dao.EmployeeDAO;
+import model.Employee;
+
+@RestController
+public class MainRESTController {
+ 
+    @Autowired
+    private EmployeeDAO employeeDAO;
+ 
+    @RequestMapping("/")
+    @ResponseBody
+    public String welcome() {
+        return "Welcome to Spring Boot + REST + JWT Example.";
+    }
+ 
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "{greeting: 'Hello'}";
+    }
+ 
+    // URL:
+    // http://localhost:8080/employees
+    @RequestMapping(value = "/employees", //
+            method = RequestMethod.GET, //
+            produces = { MediaType.APPLICATION_JSON_VALUE, //
+                    MediaType.APPLICATION_XML_VALUE })
+    @ResponseBody
+    public ArrayList<Employee> getEmployees() {
+        ArrayList<Employee> list = employeeDAO.getAllEmployees();
+        return list;
+    }
+ 
+    // URL:
+    // http://localhost:8080/employee/{empNo}
+    @RequestMapping(value = "/employee/{empNo}", //
+            method = RequestMethod.GET, //
+            produces = { MediaType.APPLICATION_JSON_VALUE, //
+                    MediaType.APPLICATION_XML_VALUE })
+    @ResponseBody
+    public Employee getEmployee(@PathVariable("empNo") String empNo) {
+        return employeeDAO.getEmployee(empNo);
+    }
+ 
+    // URL:
+    // http://localhost:8080/employee
+ 
+    @RequestMapping(value = "/employee", //
+            method = RequestMethod.POST, //
+            produces = { MediaType.APPLICATION_JSON_VALUE, //
+                    MediaType.APPLICATION_XML_VALUE })
+    @ResponseBody
+    public Employee addEmployee(@RequestBody Employee emp) {
+ 
+        System.out.println("(Service Side) Creating employee: " + emp.getEmpNo());
+ 
+        return employeeDAO.addEmployee(emp);
+    }
+ 
+    // URL:
+    // http://localhost:8080/employee
+    @RequestMapping(value = "/employee", //
+            method = RequestMethod.PUT, //
+            produces = { MediaType.APPLICATION_JSON_VALUE, //
+                    MediaType.APPLICATION_XML_VALUE })
+    @ResponseBody
+    public Employee updateEmployee(@RequestBody Employee emp) {
+ 
+        System.out.println("(Service Side) Editing employee: " + emp.getEmpNo());
+ 
+        return employeeDAO.updateEmployee(emp);
+    }
+ 
+    // URL:
+    // http://localhost:8080/employee/{empNo}
+    @RequestMapping(value = "/employee/{empNo}", //
+            method = RequestMethod.DELETE, //
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    @ResponseBody
+    public void deleteEmployee(@PathVariable("empNo") String empNo) {
+ 
+        System.out.println("(Service Side) Deleting employee: " + empNo);
+ 
+        employeeDAO.deleteEmployee(empNo);
+    }
+ 
+}
