4f30c3fb8f6aa41c15010cb8238d9a1cf69df82e
thien29797 <thien2971997@gmail.com>
2019-06-18 16:53:31 +0700

    add encode/decode functions

--- SpringBackend/src/main/java/thiendang/com/sbjwt/service/UserService.java
+++ SpringBackend/src/main/java/thiendang/com/sbjwt/service/UserService.java
@@ -5,9 +5,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.axis.encoding.Base64;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
@@ -20,50 +22,65 @@ public class UserService{
 	public static List<User> listUser = new ArrayList<User>();	
 	private static User user;
 	
-	private static void objectOutputUser() {
-		try {
-		    
-		   FileOutputStream fos = new FileOutputStream("/home/ddthien/userdata.txt");
-		   ObjectOutputStream oos = new ObjectOutputStream(fos);
-		    
-		   User u[] = { new User  (1, "thien", "123456"),
-				   new User  (2, "nhat", "123456")
-		   };
-		   
-		   oos.writeObject(u);
-		   System.out.println("Write successfully");
-		   
-		   fos.close();
-		   oos.close();
-		 } 
-		 catch (IOException ex) {
-			 System.out.println("Write file error: " +ex);
-		 }
-		 
-	}	
-	
-	private static void objectInputUser() {
-		try {
-		    
-			  FileInputStream fis = new FileInputStream("/home/ddthien/userdata.txt");
-			  ObjectInputStream ois = new ObjectInputStream(fis);
-		    
-			  User uArr[] = (User[]) ois.readObject();
-			  for (User u : uArr){
-			      user = new User(u.getId(), u.getUsername(), u.getPassword());
-			      user.setRoles(new String[] { "ROLE_ADMIN" });
-			      listUser.add(user);
-			      
+	//Encode user info
+		public static String encodeString(String text) throws UnsupportedEncodingException{
+			byte[] bytes = text.getBytes();
+			String encodeString = Base64.encode(bytes);
+			return encodeString;
+		}
+		
+		//Decode user info
+		public static String decodeString(String encodeString) throws UnsupportedEncodingException {
+			byte[] decodeBytes = Base64.decode(encodeString);
+			String str = new String(decodeBytes);
+			return str;
+		}
+		
+		private static void objectOutputUser() {
+			try {
+			    
+			   FileOutputStream fos = new FileOutputStream("/home/ddthien/userdata.txt");
+			   ObjectOutputStream oos = new ObjectOutputStream(fos);
+			    
+			   User u[] = { new User  (1, encodeString("thien"), encodeString("123456")),
+					   new User  (2, encodeString("nhat"), encodeString("123456"))
+			   };
+			   
+			   oos.writeObject(u);
+			   System.out.println("Write successfully");
+			   
+			   fos.close();
+			   oos.close();
+			 } 
+			 catch (IOException ex) {
+				 System.out.println("Write file error: " +ex);
+			 }
+			 
+		}	
+		
+		private static void objectInputUser() {
+			try {
+			    
+				  FileInputStream fis = new FileInputStream("/home/ddthien/userdata.txt");
+				  ObjectInputStream ois = new ObjectInputStream(fis);
+			    
+				  User uArr[] = (User[]) ois.readObject();
+				  for (User u : uArr){
+				      user = new User(u.getId(), decodeString(u.getUsername()), 
+				    		  decodeString(u.getPassword()));
+				      user.setRoles(new String[] { "ROLE_ADMIN" });
+				      System.out.println(user);
+				      listUser.add(user);
+				      
+				  }
+			    
+			    fis.close();
+			    ois.close();
+			  } 
+			  catch (Exception ex) {
+			    System.out.println("Read File Error: " +ex);
 			  }
-		    
-		    fis.close();
-		    ois.close();
-		  } 
-		  catch (Exception ex) {
-		    System.out.println("Read File Error: " +ex);
-		  }
-	}
-	
+		}	
 	static {
 		objectOutputUser();
 		objectInputUser();				
